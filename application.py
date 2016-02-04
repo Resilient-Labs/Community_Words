4cd32ca6e018011e3a790bb422847751e350e7f1
kyang01 <kyang01@college.harvard.edu>
2016-02-04 17:55:37 -0500

    Adding basic flask template from Amazon Flask example

---
+++ application.py
@@ -0,0 +1,61 @@
+# Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import json
+
+import flask
+from flask import request, Response
+
+from boto import dynamodb2
+from boto.dynamodb2.table import Table
+from boto.dynamodb2.items import Item
+from boto.dynamodb2.exceptions import ConditionalCheckFailedException
+from boto import sns
+
+# Default config vals
+THEME = 'default' if os.environ.get('THEME') is None else os.environ.get('THEME')
+FLASK_DEBUG = 'false' if os.environ.get('FLASK_DEBUG') is None else os.environ.get('FLASK_DEBUG')
+
+# Create the Flask app
+application = flask.Flask(__name__)
+
+# Load config values specified above
+application.config.from_object(__name__)
+
+@application.route('/')
+def welcome():
+    theme = application.config['THEME']
+    return flask.render_template('index.html', theme=theme, flask_debug=application.debug)
+
+@application.route('/signup', methods=['POST'])
+def signup():
+    signup_data = dict()
+    print type(request.form)
+    for item in request.form:
+        signup_data[item] = request.form[item]
+    print type(signup_data)
+
+    try:
+        store_in_dynamo(signup_data)
+        publish_to_sns(signup_data)
+    except Exception:
+        print(signup_data)
+        return Response(request.form, status=404, mimetype='application/json')
+
+    return Response(json.dumps(signup_data), status=201, mimetype='application/json')
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')
